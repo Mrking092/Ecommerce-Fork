4aad46bef178f8e51324d42128d002473380c765
Mohammed Wael <85469772+MohammedWael101@users.noreply.github.com>
2024-09-30 23:42:40 +0000

    starting header

--- my-app/src/Components/zeroz/Header/Header.js
+++ my-app/src/Components/zeroz/Header/Header.js
@@ -1,19 +1,60 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBagShopping} from '@fortawesome/free-solid-svg-icons'
 import siteLogo from '../../Imgs/site-logo.png'
+import HeaderStyle from './HeaderStyle.css'
 
 export default function Header(){
     return(
         <>
-            <div className='bg-gray-100'>
-                <h5 className='text-gray-400 text-center text-sm p-2'>
+            <div className='bg-gray-100 w-full'>
+                <h5 className='text-gray-400 text-center text-xs p-3'>
                     Free Express Shipping on all orders with all duties included
                 </h5>
             </div>  
-            <div className="header">
+            <div className="header p-9 pe-0 flex ">
                 <button>
+                    <a href='#'>
                     <img src={siteLogo} alt="site-logo"/>
+                    </a>
                 </button>
-            </div>    
+                <div className='headerLinks w-full'>
+                    <ul className=' flex text-gray-400 font-medium relative'>
+                        <li>
+                            <a className='pe-6' href='#'>MEN</a>
+                        </li>
+                        <li>
+                            <a className='pe-6' href='#'>WOMEN</a>
+                        </li>
+                        <li>
+                            <a className='pe-6' href='#'>COLLECTION</a>
+                        </li>
+                        <li>
+                            <a className='pe-6' href='#'>LOOKBOOK</a>
+                        </li>
+                        <li>
+                            <a className='pe-6' href='#'>SALE</a>
+                        </li>
+                        <div className='ourStory flex'>
+                            <li>
+                                <a href='#' className=' whitespace-nowrap pe-6'>OUR STORY</a>
+                            </li>    
+                            <li>
+                                <a href='#' className='pe-6'>CONTACT</a>
+                            </li>   
+                            <li>
+                                <a href='#' className='pe-8 text-black text-3xl'>
+                                    <FontAwesomeIcon icon={faBagShopping}/>
+                                </a>
+                            </li>   
+                            <li>
+                                <a href='#' className='pe-6'>
+
+                                </a>
+                            </li>   
+                        </div>
+                    </ul>
+                </div>    
+            </div>
         </>
     )
 }
