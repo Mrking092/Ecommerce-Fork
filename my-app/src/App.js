73e936cd63f0ff1063b9e9ff19d9f239ebf9289d
Mohammed Wael <85469772+MohammedWael101@users.noreply.github.com>
2024-09-30 17:11:15 +0000

    comitting fontAwsome

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/zeroz/Header/Header';
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
     {"Frieza is gay"}
+=======
+      <Header/>
+>>>>>>> 10b3f05 (hello)
     </>
   );
 }
--- my-app/src/Components/zeroz/Header/Header.js
+++ my-app/src/Components/zeroz/Header/Header.js
@@ -1,1 +1,19 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import siteLogo from '../../Imgs/site-logo.png'
+
+export default function Header(){
+    return(
+        <>
+            <div className='bg-gray-100'>
+                <h5 className='text-gray-400 text-center text-sm p-2'>
+                    Free Express Shipping on all orders with all duties included
+                </h5>
+            </div>  
+            <div className="header">
+                <button>
+                    <img src={siteLogo} alt="site-logo"/>
+                </button>
+            </div>    
+        </>
+    )
+}
