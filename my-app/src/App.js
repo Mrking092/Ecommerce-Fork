1ee02d3ad56d43b4a9fe5e5320e4c7e25a780563
Mrking092 <98407602+Mrking092@users.noreply.github.com>
2024-10-01 14:41:10 +0000

    Fixing problem (yarn)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import menPage from "./Components/mrking/Men/menPage"
 
 export default function App() {
   return (
     <>
-    {"hello"}
+      <menPage/>
     </>
   );
 }
---
+++ my-app/src/Components/mrking/Men/menPage.js
@@ -0,0 +1,5 @@
+export default function menPage(){
+    return(
+            <h1>Men Page</h1>
+    )
+}
