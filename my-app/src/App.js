b67e9f61f9e388ae2169ac1379dcb530a6736b36
Mrking092 <98407602+Mrking092@users.noreply.github.com>
2024-10-03 13:03:25 +0000

    First Step

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import menPage from "./Components/mrking/Men/menPage"
-
+import MenPage from "./Components/mrking/Men/menPage"
 export default function App() {
   return (
     <>
-      <menPage/>
+      <MenPage/>
     </>
   );
 }
--- my-app/src/Components/mrking/Men/menPage.js
+++ my-app/src/Components/mrking/Men/menPage.js
@@ -1,5 +1,15 @@
+import "./menPage.css"
+
 export default function menPage(){
     return(
-            <h1>Men Page</h1>
+        <div className="" style={{ backgroundColor:"#f1f1ef",borderColor:"#f1f1ef",borderWidth:"50px 20px",margin:"0 auto",minWidth:"300px",maxWidth:"1300px"}}>
+            <div className="m-auto" style={{backgroundColor:"white",width: "100%",height:"500px"}}>
+                <h1 className="p-10 text-7xl font-semibold" style={{color:"#6e7051"}}>Men</h1>
+                <div className="filterNSort">
+                    <div className="filter"></div>
+                    <div className="sort"></div>
+                </div>
+            </div>
+        </div>
     )
 }
