448415388699dbf12240ddec06d277a81c27df12
Mohammed Wael <mohammedsama2017@gmail.com>
2024-11-21 01:02:25 +0200

    check 90%

--- my-app/src/Components/zeroz/Checkout/Checkout.js
+++ my-app/src/Components/zeroz/Checkout/Checkout.js
@@ -1,9 +1,10 @@
+import CheckoutStyle from './CheckoutStyle.css'
 import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import siteLogo from '../../Imgs/zeroz/site-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDolly, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faDolly, faLock, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Checkout() {
@@ -22,17 +23,18 @@ export default function Checkout() {
     }
     fetchCounties();
     const data = JSON.parse(window.localStorage.getItem("item"));
-    console.log();
+    const discount = JSON.parse(window.localStorage.getItem("discount"));
+    // console.log(((1-(discount / 100)) * data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 ).toFixed(2)).toFixed(2));
     return(
         <>
             <a className='flex justify-center py-[1%] cursor-pointer'>
                 <img src={siteLogo}/>
             </a>
-            <div className='bg-[#f1f1ef] flex justify-center items-center h-[fit-content] px-[15%] py-[2%] min-w-fit'>
-                <div className='bg-white w-[100%] h-[100%] my-[3%] px-[5%] min-w-fit'>
+            <div id='checkoutMainPageContainer'  className='bg-[#f1f1ef] flex justify-center items-center h-[fit-content] px-[15%] py-[2%] min-w-fit'>
+                <div className='bg-white w-[100%] h-[100%] my-[3%] px-[5%]'>
                     <h1 className='text-7xl font-semibold text-center py-[5%]'>Checkout</h1>
-                    <div className='flex gap-x-[5%]'>
-                        <form className='w-[55%]'>
+                    <div id='cheackoutContainer' className='flex gap-x-[5%]'>
+                        <form id='checkoutForm' className='w-[55%]'>
                             <h3 className='border-b w-fit border-[#ddd] py-[2%] text-2xl font-bold'>Customer information</h3>
                             <div className='mt-[3%]'>
                                 <input required type='text' placeholder='Username or Email Address *' className='border p-[1.5%] rounded text-lg text-black w-full border-[#ddd]'/>
@@ -65,7 +67,7 @@ export default function Checkout() {
                             <h3 className='border-b border-[#ddd] py-[2%] text-2xl font-bold'>Additional information</h3>
                             <textarea className='border mt-[3%] p-[1.5%] rounded text-lg text-black w-full border-[#ddd]' placeholder='Notes about your order, e.g. spectial notes for delivery.'/>
                             <h3 className='py-[2%] text-2xl font-bold'>payments</h3>
-                            <div className='mt-[1%] px-[2%] mb-[10%]'>
+                            <div className='mt-[1%] mb-[10%]'>
                                 <div className='flex p-[3%] gap-x-[2%] border-t-4 border-[#6e7051] bg-[#f7f6f7] '>
                                     <FontAwesomeIcon className='text-2xl mt-1.5 text-[#6e7051]' icon={faDolly} />
                                     <p className='text-md font-semibold text-[#6e7051]'>Sorry, it seems that there are no available payment methods.
@@ -74,17 +76,50 @@ export default function Checkout() {
                                 </div>
                                 <button className=' bg-[#6e7051] tracking-widest	 font-semibold text-[1rem] mt-[2%] text-white p-3 w-[100%] duration-[0.8s] hover:bg-[#262b2c]'><a className='flex justify-center items-center gap-x-[2%]' href='#'>
                                 <FontAwesomeIcon className='text-sm' icon={faLock} />
-                                <p>PLACE ORDER ${data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 ).toFixed(2)}</p>
+                                <p>PLACE ORDER ${discount ? ((1-(discount / 100)) * data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 )).toFixed(2) : data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0).toFixed(2)}</p>
                                 </a></button>
                             </div>
                         </form>
 
-                        <div className='w-[45%]'>
-                        <h3 className='w-fit py-[2%] text-2xl font-bold'>Your order</h3>
+                        <div id='yourOrder' className='w-[45%] rounded'>
+                            <h3 className='w-fit py-[2%] text-2xl font-bold'>Your order</h3>
+                            <div id='yourOrderContainer' className='text-[#979a9b] font-semibold border mt-[1%] rounded text-lg w-full border-[#ddd]'>
+                                <div id='yourOrderSammary' className='hidden items-center border-b border-[#e5e7eb] ps-[4%] cursor-pointer justify-between bg-[#f1f1ef]'>
+                                    <div className='flex py-[5%] w-[75%] items-center gap-x-[2%]'>
+                                        <h6 className='whitespace-nowrap'>Show Order Summary</h6>
+                                        <FontAwesomeIcon className='duration-[0.4s]' icon={faAngleUp} />
+                                    </div>
+                                    <p className='yourOrderValue w-[25%] py-[3%] font-bold text-center min-w-fit'>${discount ? ((1-(discount / 100)) * data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 )).toFixed(2) : data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0).toFixed(2)}</p>
+                                </div>
+                                <div id='yourOrderDiv' className=''>
 
+                                    <div id='yourOrderTitle' className='flex ps-[2%] py-[3%] justify-between items-center border-b'>
+                                        <p className='w-[75%] min-w-fit'>Product</p>
+                                        <p className='yourOrderValue w-[25%] min-w-fit text-center'>Subtotal</p>
+                                        </div>
+                                    <div className=''>
+                                        {data.map(item =>
+                                            <div className='flex ps-[4%] justify-between items-center border-b'>
+                                                <div className='flex py-[3%] w-[75%] items-center gap-x-[2%]'>
+                                                    <img src={`${process.env.PUBLIC_URL}${item.img}`} className='w-[75px] rounded h-[75px]'/>
+                                                    <p className='text-[1rem]'>{item.name} x {item.shoesNumber}</p>
+                                                </div>
+                                                <p className='yourOrderValue w-[25%] text-center'>{item.price.new}</p>
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className='flex ps-[4%] py-[3%] justify-between items-center border-b'>
+                                        <h6 className=' w-[75%] min-w-fit'>Subtotal</h6>
+                                        <p className='yourOrderValue w-[25%] text-center min-w-fit'>${data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 ).toFixed(2)}</p>
+                                    </div>
+                                    <div className='flex ps-[4%] py-[3%] justify-between items-center border-b'>
+                                        <h6 className=' w-[75%] font-bold min-w-fit'>Total</h6>
+                                        <p className='yourOrderValue w-[25%] font-bold text-center min-w-fit'>${discount ? ((1-(discount / 100)) * data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0 )).toFixed(2) : data.reduce((acc,item) => acc + +item.price.new.replace('$','') ,0).toFixed(2)}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
